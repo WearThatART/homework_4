7471ccb26fa3658f7bd80757e546469236c2ea51
Cotimia <wearthatart@gmail.com>
2015-09-21 18:11:01 -0700

    lessons learned
    testing

---
+++ homework_4/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  homework_4
+//
+//  Created by Arturo Duran on 9/21/15.
+//  Copyright © 2015 Mendillo_maria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var pog: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func tshirt(sender: UITapGestureRecognizer) {
+    }
+
+    @IBAction func SetDefaultLabelText(sender: UITapGestureRecognizer) {
+    }
+}
+
